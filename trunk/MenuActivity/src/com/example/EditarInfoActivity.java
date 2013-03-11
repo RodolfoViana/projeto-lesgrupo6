c09a45546098bdd18df0bd1e73b54ecdfc4eabb4
brunnaamorim@gmail.com <brunnaamorim@gmail.com@544ecda6-c2a7-dcc9-1ffd-f5573368ba79>
2013-03-11 23:49:35 +0000

    Funcionalidade: Adicionar novos modelos de carro a lista. Correção do bug B9: cadastrar novo modelo do carro. Tratamento dos dados

--- trunk/MenuActivity/src/com/example/EditarInfoActivity.java
+++ trunk/MenuActivity/src/com/example/EditarInfoActivity.java
@@ -1,51 +1,85 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class EditarInfoActivity extends Activity{
 	
 	private double desempenho;
-	 private Button btnSubmit;
-	 private TextView visuDesempenho;
+	private String combustivelPorKm;
+	private String modelo;
+	private List<String> valuesList;
+	private List<Double> desempenhoList;
+	private Spinner spinnerCarros;
+	private int checkedItem;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.editar_info);
+        setContentView(R.layout.editar_info);        
+
+        combustivelPorKm = (String) getIntent().getSerializableExtra("desempenho");
+        modelo = (String) getIntent().getSerializableExtra("modeloCarro");
         
-        //visuDesempenho = (TextView) findViewById(R.id.VerDesempenho);
+        povoaLista();
+        desempenhos();
+        spinnerCarros = (Spinner) findViewById(R.id.spinner1);
         
-        final Spinner spinnerCarros = (Spinner) findViewById(R.id.spinner1);
         
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.
-        		createFromResource(this, R.array.lista_carros, android.R.layout.simple_spinner_item);
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.
+//        		createFromResource(this, R.array.lista_carros, android.R.layout.simple_spinner_item);
+//      
+        ArrayAdapter<Object> adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, android.R.id.text1, valuesList.toArray());      
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        
         spinnerCarros.setAdapter(adapter);
         
         spinnerCarros.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				checkedItem = spinnerCarros.getSelectedItemPosition();
+				if (checkedItem != 0) {
+					Toast.makeText(EditarInfoActivity.this, "Modelo: " + valuesList.get(checkedItem) + " Desempenho: " + desempenhoList.get(checkedItem) + "km/l", Toast.LENGTH_LONG).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				
+			}
+		});
+        
+        /*spinnerCarros.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+				checkedItem = spinnerCarros.getSelectedItemPosition();
+				Toast.makeText(EditarInfoActivity.this, "Modelo: " + valuesList.get(checkedItem) + " Desempenho: " + desempenhoList.get(checkedItem), Toast.LENGTH_LONG).show();
+            } 
+		});*/
+        /*spinnerCarros.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
 			public void onItemSelected(AdapterView parent, View arg1,
 					int arg2, long arg3) {
-				// TODO Auto-generated method stub
+				//int checkedItem = spinnerCarros.getSelectedItemPosition();
 				
 				if(parent.getSelectedItem().toString().equals("Carros")){
 					setConsumo(0.0);
 				}else if(parent.getSelectedItem().toString().equals("Celta")){
 					setConsumo(10.7);
-					 verDesempenho(parent);
+					verDesempenho(parent);
 				}else if(parent.getSelectedItem().toString().equals("Gol 1.0")){
 					setConsumo(9.8);
 					verDesempenho(parent);
@@ -75,28 +109,25 @@ public class EditarInfoActivity extends Activity{
 					verDesempenho(parent);
 				}
 				
-				 
-				// visuDesempenho.setText("" + getDesempenho());
 			}
 
 			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-        	
-        	
-		});
+			public void onNothingSelected(AdapterView<?> arg0) {}
+		}); */
         
-       
-       
-		
 		Button editarInfoButton = (Button) findViewById(R.id.editarInfoButton);
 		editarInfoButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 editarInfoCarro();
             }
 
+        });
+		
+		Button okBt = (Button) findViewById(R.id.editarInfoOkButton);
+		okBt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	menuPrincipal();
+            }
         });
 		
 		Button cancelar = (Button) findViewById(R.id.cancelarButton);
@@ -106,11 +137,6 @@ public class EditarInfoActivity extends Activity{
             }
         });
 		
-	}
-	
-	private void listaModelos() {
-		Intent lista = new Intent(this, ModelosActivity.class);
-        startActivity(lista);
 	}
 	
 	private void editarInfoCarro() {
@@ -126,10 +152,54 @@ public class EditarInfoActivity extends Activity{
 		this.desempenho = consumo;
 	}
 	
+	@SuppressWarnings("rawtypes")
 	public void verDesempenho(AdapterView parent){
 		Toast.makeText(parent.getContext(), "Desempenho do seu carro na cidade é: " +
 				 getDesempenho() + "Km/l", Toast.LENGTH_LONG).show();
 	}
 	
-
+	private void menuPrincipal() {
+		Intent menu = new Intent(this, MenuActivity.class);
+		menu.putExtra("combustivelPorKm", String.valueOf(desempenhoList.get(this.checkedItem)));
+        startActivity(menu);
+	}
+	
+	private void povoaLista() {
+		this.valuesList = new ArrayList<String>();
+        this.valuesList.add("Carros");
+		this.valuesList.add("Celta");
+        this.valuesList.add("Gol 1.0");
+        this.valuesList.add("Gol 1.6");
+        this.valuesList.add("Uno Mille");
+        this.valuesList.add("Novo Palio 1.0");
+        this.valuesList.add("Siena 1.4");
+        this.valuesList.add("Fox 1.0");
+        this.valuesList.add("Fox 1.6");
+        this.valuesList.add("Ford Ka");
+        this.valuesList.add("Onix");
+        
+        if ((combustivelPorKm != null) && (modelo != null)) {
+        	valuesList.add(modelo.toString());
+        }
+	}
+	
+	private void desempenhos() {
+		this.desempenhoList = new ArrayList<Double>();
+		//{10.7, 9.8, 9.4, 12.0, 9.4, 12.0, 9.1, 9.4, 11.6, 10.6}
+		this.desempenhoList.add(0.0);
+		this.desempenhoList.add(10.7);
+		this.desempenhoList.add(9.8);
+		this.desempenhoList.add(9.4);
+		this.desempenhoList.add(12.0);
+		this.desempenhoList.add(9.4);
+		this.desempenhoList.add(12.0);
+		this.desempenhoList.add(9.1);
+		this.desempenhoList.add(9.4);
+		this.desempenhoList.add(11.6);
+		this.desempenhoList.add(10.6);
+		
+		if ((combustivelPorKm != null) && (modelo != null)) {
+        	this.desempenhoList.add(Double.parseDouble(combustivelPorKm));
+        }
+	}
 }
